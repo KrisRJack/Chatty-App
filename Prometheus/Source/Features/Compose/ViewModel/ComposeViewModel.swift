7d15d8db13f18302b8b1efb538f91157ac0175e1
Kris Jackson <kristopherrjackson@gmail.com>
2022-01-13 08:06:37 -0500

    Created Compose View Model

---
+++ Prometheus/Source/Features/Compose/ViewModel/ComposeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ComposeViewModel.swift
+//  Prometheus
+//
+//  Created by Kristopher Jackson on 1/13/22.
+//
+
+import Foundation
+
+final class ComposeViewModel: NSObject {
+    
+    
+    public var numberOfSections: Int { 1 }
+    public var numberOfRowsInSection: [Int : Int] { [0 : 1] }
+    
+    
+    
+}
