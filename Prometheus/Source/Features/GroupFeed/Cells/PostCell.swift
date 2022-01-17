2c424c744acc8cef69e3887dc534b0547d3f629d
Kris Jackson <kristopherrjackson@gmail.com>
2022-01-17 12:47:29 -0600

    Added clear background to PostCell

--- Prometheus/Source/Features/GroupFeed/Cells/PostCell.swift
+++ Prometheus/Source/Features/GroupFeed/Cells/PostCell.swift
@@ -21,6 +21,8 @@ final class PostCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
         contentView.fill(with: postView, insets: UIEdgeInsets(top: 0, left: 0, bottom: -8, right: 0), considerMargins: true)
     }
     
