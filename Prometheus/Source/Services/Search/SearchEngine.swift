5a86a4167c2b9ecc92ff74defda3c4469823c369
Kris Jackson <kristopherrjackson@gmail.com>
2022-01-23 10:21:10 -0600

    Added search engine 'add' method

---
+++ Prometheus/Source/Services/Search/SearchEngine.swift
@@ -0,0 +1,62 @@
+//
+//  Search.swift
+//  Prometheus
+//
+//  Created by Kristopher Jackson on 1/22/22.
+//
+
+import Foundation
+import AlgoliaSearchClient
+
+final class SearchEngine: NSObject {
+    
+    
+    private let client: SearchClient!
+    
+    
+    override init() {
+        client = SearchClient(
+            appID: API_Keys.Algolia.APP_ID,
+            apiKey: API_Keys.Algolia.API_KEY
+        )
+        super.init()
+    }
+    
+    
+    public func add<T: Encodable>(
+        
+        object: T,
+        withID id: String,
+        toIndex index: SearchEngine.Index,
+        _ completion: @escaping (_ response: WaitableWrapper<ObjectRevision>?, _ error: Error?) -> Void
+        
+    ) {
+        
+        let objectID = ObjectID(rawValue: id)
+        let indexName = IndexName(rawValue: index.rawValue)
+        let index = client.index(withName: indexName)
+        
+        // If an object already exists, replace it.
+        index.replaceObject(withID: objectID, by: object) { result in
+            switch result {
+            case .success(let response):
+                completion(response, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+        
+    }
+    
+}
+
+
+extension SearchEngine {
+    
+    public enum Index: String {
+        case users = "users"
+        case posts = "posts"
+        case groups = "groups"
+    }
+
+}
